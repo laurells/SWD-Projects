6c3de9f6770288adfed040e8b849cfb0acf23328
Laurels Echichinwo <laurels.echichinwo@stu.cu.edu.ng>
2025-05-10 11:52:09 +0100

    Update

--- hello_world/obj/Debug/net9.0/hello_world.AssemblyInfo.cs
+++ hello_world/obj/Debug/net9.0/hello_world.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hello_world")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19af589ad430b69f85b6d62293274a31d6c8bf53")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1a3a01ce70a3249c69b73cc1de510bbc73ef695")]
 [assembly: System.Reflection.AssemblyProductAttribute("hello_world")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hello_world")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
